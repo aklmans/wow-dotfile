a0145acd6a08f4750b073a6af7dca79d2eb111b3
Aklman <aklman2018@gmail.com>
2024-05-29 22:42:08 +0800

    Setup IDEAVIM

---
+++ installs/ideavim.sh
@@ -0,0 +1,9 @@
+echo "Installing IdeaVIM: "
+
+echo "Setting up IdeaVIM"
+
+mv $HOME/.ideavimrc $HOME/.ideavimrc.backup
+
+cp ../ideavim/ideavimrc $HOME/.ideavimrc
+
+echo "Finished installing IdeaVIM"
