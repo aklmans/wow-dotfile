18a7f876aff7a59f690cde272a94db67bb05573e
Aklman <aklman2018@gmail.com>
2024-06-01 13:48:56 +0800

    Update Yazi config

--- installs/yazi.sh
+++ installs/yazi.sh
@@ -4,6 +4,8 @@ echo "Installing Dependencies"
 
 brew install yazi ffmpegthumbnailer unar jq mpv poppler fd ripgrep fzf zoxide font-symbols-only-nerd-font
 
+brew install bat  exiftool tree glow imagemagick pandoc sqlite smali miller transmission-cli woff2
+
 echo "Setting up Yazi"
 
 mv $HOME/.config/yazi $HOME/.config/yazi.backup
