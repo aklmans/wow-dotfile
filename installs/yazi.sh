c3831a797847409cd36df62a22c68309ccbf8374
Aklman <aklman2018@gmail.com>
2024-08-23 11:16:34 +0800

    Update Yazi Config

--- installs/yazi.sh
+++ installs/yazi.sh
@@ -4,16 +4,19 @@ echo "Installing Dependencies"
 
 brew install yazi ffmpegthumbnailer unar jq mpv poppler fd ripgrep fzf zoxide font-symbols-only-nerd-font
 
-brew install bat  exiftool tree glow imagemagick pandoc sqlite smali miller transmission-cli woff2
+brew install bat  exiftool tree glow imagemagick pandoc sqlite smali miller transmission-cli woff2 rich
 
 echo "Setting up Yazi"
 
+mv $HOME/.local/state/yazi $HOME/.local/state/yazi.backup
 mv $HOME/.config/yazi $HOME/.config/yazi.backup
 cp -r ../yazi $HOME/.config/yazi
-rm -rf $HOME/.local/state/yazi/packages
+
 
 # https://github.com/AnirudhG07/awesome-yazi
 echo "Installing plugins"
+# https://github.com/AnirudhG07/rich-preview.yazi
+ya pack -a AnirudhG07/rich-preview
 ya pack -a dedukun/relative-motions
 ya pack -a dedukun/bookmarks
 ya pack -a Reledia/glow
@@ -25,11 +28,12 @@ ya pack -a Rolv-Apneseth/bypass
 ya pack -a Rolv-Apneseth/starship
 ya pack -a Reledia/hexyl
 ya pack -a kirasok/epub-preview
-# https://github.com/Reledia/miller.yazi
-ya pack -a Reledia/miller
 # https://github.com/yazi-rs/plugins/tree/main
 ya pack -a yazi-rs/plugins:max-preview
 ya pack -a yazi-rs/plugins:chmod
 ya pack -a yazi-rs/plugins:smart-filter
+
+# echo "updating plugins"
+# ya pack -u
 
 echo "Finished installing Yazi"
