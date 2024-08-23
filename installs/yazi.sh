b3e0ea3b5890ae9eb71baaba8d1f4a12c302669d
Aklman <aklman2018@gmail.com>
2024-08-23 10:25:45 +0800

    Update Yazi Config

--- installs/yazi.sh
+++ installs/yazi.sh
@@ -10,7 +10,31 @@ echo "Setting up Yazi"
 
 mv $HOME/.config/yazi $HOME/.config/yazi.backup
 cp -r ../yazi $HOME/.config/yazi
+rm -rf $HOME/.local/state/yazi/packages
+
+# https://github.com/AnirudhG07/awesome-yazi
+echo "Installing plugins"
+ya pack -a dedukun/relative-motions
+ya pack -a dedukun/bookmarks
+ya pack -a Reledia/glow
+ya pack -a Sonico98/exifaudio
+ya pack -a ndtoan96/ouch
+# https://github.com/lpnh/fg.yazi
+ya pack -a lpnh/fg
+ya pack -a Rolv-Apneseth/bypass
+ya pack -a Rolv-Apneseth/starship
+ya pack -a Reledia/hexyl
+ya pack -a kirasok/epub-preview
+# https://github.com/Reledia/miller.yazi
+ya pack -a Reledia/miller
+# https://github.com/Urie96/preview.yazi
+ya pack -a Urie96/preview
+# https://github.com/yazi-rs/plugins/tree/main
+ya pack -a yazi-rs/plugins:max-preview
+ya pack -a yazi-rs/plugins:chmod
+ya pack -a yazi-rs/plugins:smart-filter
+
+echo "updating plugins"
+ya pack -u
 
 echo "Finished installing Yazi"
-
-yazi
--- yazi/init.lua
+++ yazi/init.lua
@@ -1,74 +1,61 @@
--- -- https://yazi-rs.github.io/docs/tips#show-symlink-in-status-bar
--- function Status:name()
--- 	local h = cx.active.current.hovered
--- 	if not h then
--- 		return ui.Span ''
--- 	end
+-- https://github.com/Rolv-Apneseth/.dotfiles/blob/1d230af142770f8056375c73724908de84499f93/yazi/.config/yazi/init.lua
+-- PLUGIN SETUP/CONFIG
+--- Require plugin, giving a warning notification if a plugin is not found
+--- @param plugin_name string
+---@return nil|any
+local function require_plugin(plugin_name)
+    local status_ok, plugin = pcall(require, plugin_name)
 
--- 	local linked = ''
--- 	if h.link_to ~= nil then
--- 		linked = ' -> ' .. tostring(h.link_to)
--- 	end
--- 	return ui.Span(' ' .. h.name .. linked)
--- end
+    if not status_ok then
+        ya.notify({
+            title = "Plugins",
+            content = "Could not find plugin: " .. plugin_name,
+            timeout = 5,
+        })
+        return nil
+    end
 
--- -- https://yazi-rs.github.io/docs/tips#show-usergroup-of-files-in-status-bar
--- function Status:owner()
--- 	local h = cx.active.current.hovered
--- 	if h == nil or ya.target_family() ~= 'unix' then
--- 		return ui.Line {}
--- 	end
+    return plugin
+end
 
--- 	return ui.Line {
--- 		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg 'magenta',
--- 		ui.Span ':',
--- 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg 'magenta',
--- 		ui.Span ' ',
--- 	}
--- end
+-- OVERRIDE DEFAULT UI
+-- Give some extra space for footer and header + cool columns
+local old_build = Tab.build
+Tab.build = function(self, ...)
+    local bar = function(c, x, y)
+        if x <= 0 or x == self._area.w - 1 then
+            return ui.Bar(ui.Rect.default, ui.Bar.TOP)
+        end
 
--- function Status:render(area)
--- 	self.area = area
+        return ui.Bar(
+            ui.Rect({
+                x = x,
+                y = math.max(0, y),
+                w = ya.clamp(0, self._area.w - x, 1),
+                h = math.min(1, self._area.h),
+            }),
+            ui.Bar.TOP
+        ):symbol(c)
+    end
 
--- 	local left = ui.Line { self:mode(), self:size(), self:name() }
--- 	local right = ui.Line {
--- 		self:owner(),
--- 		self:permissions(),
--- 		self:percentage(),
--- 		self:position(),
--- 	}
--- 	return {
--- 		ui.Paragraph(area, { left }),
--- 		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
--- 		table.unpack(Progress:render(area, right:width())),
--- 	}
--- end
+    local c = self._chunks
+    self._chunks = {
+        c[1]:padding(ui.Padding.y(1)),
+        c[2]:padding(ui.Padding(c[1].w > 0 and 0 or 1, c[3].w > 0 and 0 or 1, 1, 1)),
+        c[3]:padding(ui.Padding.y(1)),
+    }
 
-function Manager:render(area)
-	local chunks = self:layout(area)
+    local style = THEME.manager.border_style
+    self._base = ya.list_merge(self._base or {}, {
+        --[[ ui.Border(self._area, ui.Border.ALL):type(ui.Border.ROUNDED):style(style), ]]
+        ui.Bar(self._chunks[1], ui.Bar.RIGHT):style(style),
+        ui.Bar(self._chunks[3], ui.Bar.LEFT):style(style),
 
-	local bar = function(c, x, y)
-		x, y = math.max(0, x), math.max(0, y)
-		return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
-			:symbol(c)
-	end
+        bar("┬", c[1].right - 1, c[1].y),
+        bar("┴", c[1].right - 1, c[1].bottom - 1),
+        bar("┬", c[2].right, c[2].y),
+        bar("┴", c[2].right, c[1].bottom - 1),
+    })
 
-	return ya.flat {
-		-- Borders
-		ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-		ui.Bar(chunks[1], ui.Bar.RIGHT),
-		ui.Bar(chunks[3], ui.Bar.LEFT),
-
-		bar("┬", chunks[1].right - 1, chunks[1].y),
-		bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
-		bar("┬", chunks[2].right, chunks[2].y),
-		bar("┴", chunks[2].right, chunks[1].bottom - 1),
-
-		-- Parent
-		Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-		-- Current
-		Current:render(chunks[2]:padding(ui.Padding.y(1))),
-		-- Preview
-		Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
-	}
+    old_build(self, ...)
 end
